f90bc294055c4bdd7de8b04f10fcef00040072fa
IvanFrezzaJr <ivanfrezza@gmail.com>
2020-09-22 18:26:58 +0100

    first commit

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,31 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
+
+    uploadedFiles.forEach(uploadedFile => {
+      data.append('file', uploadedFile.file);
+      data.append('name', uploadedFile.name);
+      data.append('readableSize', uploadedFile.readableSize);
+    });
 
     // TODO
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const filesSanitised = files.map(file => ({
+      file,
+      name: file.name,
+      readableSize: String(file.size),
+    }));
+
+    setUploadedFiles(filesSanitised);
   }
 
   return (
